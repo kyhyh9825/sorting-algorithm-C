c2a73637741fcda91a9435ce268c9154ce77ed0e
kyhyh9825 <75295040+kyhyh9825@users.noreply.github.com>
2025-12-01 21:07:53 +0900

    select포인터 오류 수정

--- library/selection_sort.c
+++ library/selection_sort.c
@@ -3,31 +3,31 @@
  * @brief 선택 정렬 구현부
  */
 
- #include <stdio.h>
- #include "../library/sorting.h"
+#include <stddef.h>
+#include "../library/sorting.h"
 
- void selection_sort(void *arr, size_t num_of_elements, size_t size_of_element, int (*cmp_func_ptr)(const void *a_ptr, const void *b_ptr))
- {
+void selection_sort(void *arr, size_t num_of_elements, size_t size_of_element, int (*cmp_func_ptr)(const void *a_ptr, const void *b_ptr))
+{
     if (SORT_UNLIKELY(arr == NULL || num_of_elements <= 1 || size_of_element == 0))
     {
         return;
     }
 
-    void *select = arr;
-    void *current = arr;
-    for (size_t i = 0; i < num_of_elements; i++)
+    void *current = (char *)arr;
+    for (size_t i = 0; i < num_of_elements - 1; i++)
     {
-        void *scan = (char *)select + size_of_element;
+        void *select = (char *)current;
+        void *scan = (char *)current + size_of_element;
         for (size_t j = i + 1; j < num_of_elements; j++)
         {
             if (cmp_func_ptr(select, scan) > 0)
             {
                 select = scan;
             }
-            generic_swap(current, select, size_of_element); 
             scan = (char *)scan + size_of_element;
         }
-        select = (char *)select + size_of_element;
+        generic_swap(current, select, size_of_element);
+
         current = (char *)current + size_of_element;
     }
 }
