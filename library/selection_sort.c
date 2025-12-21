77051e5ac8f1df3fe931d57994b80fe13c680ee2
kyhyh9825 <75295040+kyhyh9825@users.noreply.github.com>
2025-12-21 17:33:40 +0900

    자료형 큰 경우에서 버퍼 할당 로직 수정(반복 할당 금지)

--- library/selection_sort.c
+++ library/selection_sort.c
@@ -13,6 +13,24 @@ void selection_sort(void *arr, size_t num_of_elements, size_t size_of_element, i
         return;
     }
 
+    char stack_buf[SWAP_BUF_SIZE];
+    void *tmp_buf = NULL;
+    int is_heap = 0;
+
+    if (SORT_LIKELY(size_of_element <= SWAP_BUF_SIZE))
+    {
+        tmp_buf = stack_buf;
+    }
+    else
+    {
+        tmp_buf = malloc(size_of_element);
+        if (SORT_UNLIKELY(tmp_buf == NULL))
+        {
+            return;
+        }
+        is_heap = 1;
+    }
+
     void *current = (char *)arr;
     for (size_t i = 0; i < num_of_elements - 1; i++)
     {
@@ -26,8 +44,13 @@ void selection_sort(void *arr, size_t num_of_elements, size_t size_of_element, i
             }
             scan = (char *)scan + size_of_element;
         }
-        generic_swap(current, select, size_of_element);
+        generic_swap(current, select, tmp_buf, size_of_element);
 
         current = (char *)current + size_of_element;
+    }
+
+    if (is_heap)
+    {
+        free(tmp_buf);
     }
 }
--- library/sorting.h
+++ library/sorting.h
@@ -34,31 +34,15 @@
 #define SWAP_BUF_SIZE 256
 
 /* 다양한 정렬에 사용되는 swap 함수 */
-static inline void generic_swap(void *a_ptr, void *b_ptr, size_t size_of_element)
+static inline void generic_swap(void *a_ptr, void *b_ptr, void *tmp_buf, size_t size_of_element)
 {
     if (SORT_UNLIKELY(a_ptr == b_ptr))
     {
         return;
     }
-    if (SORT_LIKELY(size_of_element <= SWAP_BUF_SIZE))
-    {
-        char tmp[SWAP_BUF_SIZE];
-        memcpy(tmp, a_ptr, size_of_element);
-        memcpy(a_ptr, b_ptr, size_of_element);
-        memcpy(b_ptr, tmp, size_of_element);
-    }
-    else
-    {
-        void *tmp = malloc(size_of_element);
-        if (SORT_UNLIKELY(tmp == NULL))
-        {
-            return;
-        }
-        memcpy(tmp, a_ptr, size_of_element);
-        memcpy(a_ptr, b_ptr, size_of_element);
-        memcpy(b_ptr, tmp, size_of_element);
-        free(tmp);
-    }
+    memcpy(tmp_buf, a_ptr, size_of_element);
+    memcpy(a_ptr, b_ptr, size_of_element);
+    memcpy(b_ptr, tmp_buf, size_of_element);
 }
 
 /**
@@ -108,6 +92,12 @@ int merge_sort_multi(void *arr, size_t num_of_elements, size_t size_of_element, 
  * 
  */
 int merge_sort_pp(void *arr, size_t num_of_elements, size_t size_of_element, int (*cmp_func_ptr)(const void *a_ptr, const void *b_ptr));
+
+/**
+ * @brief 힙 정렬
+ * 
+ */
+void heap_sort(void *arr, size_t num_of_elements, size_t size_of_element, int (*cmp_func_ptr)(const void *a_ptr, const void *b_ptr));
 
 
 /**
